4bdf4adef62d76792a31a999cd8d037dd1ab397e
jbukl <148171160+jbukl@users.noreply.github.com>
2024-01-05 18:55:36 -0500

    challenge 4 done; with rpcs
    what is the point of the seq-kv??? i thought NodeIDs didn't actually return every node in the "network", rather only for the "cluster", so i used it for that initially
    but all nodes are part of the same cluster??
    you could write to the seq-kv just for the local node itself, but that proves meaningless when you can just easily store it locally instead
    as for relying on the seq-kv to sum local counters of every node, even that doesn't work, because the consistency guarantees just aren't there to update in time for the end
    this solution should almost certainly work, but obviously i didn't even need to use the seq-kv

--- maelstrom-counter/main.go
+++ maelstrom-counter/main.go
@@ -1,53 +1,31 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-func initialize_list(ctx context.Context, kv *maelstrom.KV, n *maelstrom.Node) {
-	_ = kv.CompareAndSwap(ctx, "node_list", []string{}, []string{}, true)
-
-	for {
-		x, _ := kv.Read(ctx, "node_list")
-		interface_arr := x.([]interface{})
-
-		node_list := []string{}
-		for _, y := range interface_arr {
-			node_list = append(node_list, y.(string))
-		}
-		node_list = append(node_list, n.ID())
-
-		if err := kv.CompareAndSwap(ctx, "node_list", x, node_list, false); err == nil {
-			break
-		}
-
-		time.Sleep(10 * time.Millisecond)
-	}
+func sleepChan(neighbor string, c chan string) {
+	time.Sleep(50 * time.Millisecond)
+	c <- neighbor
 }
 
-func read_nodes(ctx context.Context, kv *maelstrom.KV) []string {
-	x, _ := kv.Read(ctx, "node_list")
-	interface_arr := x.([]interface{})
-
-	node_list := []string{}
-	for _, y := range interface_arr {
-		node_list = append(node_list, y.(string))
+func initialize_list(n *maelstrom.Node, c chan string) {
+	for _, neigh := range n.NodeIDs() {
+		c <- neigh
 	}
-	return node_list
 }
 
 func main() {
 	n := maelstrom.NewNode()
-	kv := maelstrom.NewSeqKV(n)
-	ctx := context.Background()
-	node_counter := 0
+	node_counters := map[string]int{}
 	list_initialized := false
+	c := make(chan string)
+	var mu sync.Mutex
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -56,10 +34,10 @@ func main() {
 		}
 
 		delta := int(body["delta"].(float64))
-		node_counter += delta
-		if err := kv.Write(ctx, fmt.Sprintf("%s-counter", n.ID()), node_counter); err != nil {
-			return err
-		}
+
+		mu.Lock()
+		node_counters[n.ID()] += delta
+		mu.Unlock()
 
 		body["type"] = "add_ok"
 		delete(body, "delta")
@@ -74,28 +52,72 @@ func main() {
 		}
 
 		if !list_initialized {
-			initialize_list(ctx, kv, n)
+			initialize_list(n, c)
 			list_initialized = true
 		}
 
-		node_list := read_nodes(ctx, kv)
-
 		read_total := 0
-		for _, neigh := range node_list {
-			if neigh == n.ID() {
-				read_total += node_counter
-				continue
-			}
-			val, err := kv.ReadInt(ctx, fmt.Sprintf("%s-counter", neigh))
-			if err == nil {
-				read_total += val
-			}
+		mu.Lock()
+		for _, ctr := range node_counters {
+			read_total += ctr
 		}
+		mu.Unlock()
+
 		body["type"] = "read_ok"
 		body["value"] = read_total
 
 		return n.Reply(msg, body)
 	})
+
+	n.Handle("sniff_node", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		counters_copy := map[string]int{}
+		mu.Lock()
+		for k, v := range node_counters {
+			counters_copy[k] = v
+		}
+		mu.Unlock()
+
+		body["type"] = "sniff_ok"
+		body["counters"] = counters_copy
+
+		return n.Reply(msg, body)
+	})
+
+	go func() {
+		for neighbor := range c {
+			go sleepChan(neighbor, c)
+
+			body := map[string]interface{}{"type": "sniff_node"}
+
+			neigh := neighbor
+			n.RPC(neigh, body, func(msg maelstrom.Message) error {
+				var body map[string]any
+				if err := json.Unmarshal(msg.Body, &body); err != nil {
+					return err
+				}
+				v := body["counters"]
+				neigh_counters, ok := v.(map[string]interface{})
+				if !ok {
+					return nil
+				}
+
+				mu.Lock()
+				for k, v := range neigh_counters {
+					if _, ok := node_counters[k]; !ok {
+						node_counters[k] = 0
+					}
+					node_counters[k] = max(node_counters[k], int(v.(float64)))
+				}
+				mu.Unlock()
+				return nil
+			})
+		}
+	}()
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
