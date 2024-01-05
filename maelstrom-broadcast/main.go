c2887259d67fb962651dea306b383a915bc6b83e
jbukl <148171160+jbukl@users.noreply.github.com>
2024-01-04 22:05:20 -0500

    challenge 3 done

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -4,18 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+func sleepChan(neighbor string, c chan string) {
+	time.Sleep(50 * time.Millisecond)
+	c <- neighbor
+}
+
 func main() {
 	n := maelstrom.NewNode()
-	message_map := make(map[float64]bool)
+	message_map := make(map[float64]int) // message: idx in local messages list
 	messages := make([]float64, 0)
 	topology := make([]string, 0)
-
-	queue := make([]float64, 0, 10)
+	acked_map := make(map[string]int) // node_id: latest acked index into messages
+	var mu sync.Mutex
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -23,12 +30,13 @@ func main() {
 			return err
 		}
 		message := body["message"].(float64)
+		mu.Lock()
 		_, ok := message_map[message]
 		if !ok {
-			message_map[message] = true
+			message_map[message] = len(messages)
 			messages = append(messages, message)
-			queue = append(queue, message)
 		}
+		mu.Unlock()
 		resp := map[string]interface{}{
 			"type": "broadcast_ok",
 		}
@@ -52,30 +60,34 @@ func main() {
 			fmt.Printf("invalid type: %T\n", v)
 		}
 		for _, message := range recv_messages {
+			mu.Lock()
 			_, ok := message_map[message]
 			if !ok {
-				message_map[message] = true
+				message_map[message] = len(messages)
 				messages = append(messages, message)
-				queue = append(queue, message)
 			}
+			mu.Unlock()
 		}
-		return nil
+		resp := map[string]interface{}{
+			"type": "broadcast_node_ok",
+			"ack":  recv_messages[len(recv_messages)-1],
+			"node": n.ID(),
+		}
+		return n.Reply(msg, resp)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		messages := make([]float64, 0, len(message_map))
-		for k := range message_map {
-			messages = append(messages, k)
-		}
-
+		mu.Lock()
 		resp := map[string]interface{}{
 			"type":     "read_ok",
-			"messages": messages,
+			"messages": append([]float64{}, messages...),
 		}
+		mu.Unlock()
 
 		return n.Reply(msg, resp)
 	})
 
+	c := make(chan string)
 	n.Handle("topology", func(msg maelstrom.Message) error {
 
 		var body map[string]any
@@ -89,7 +101,11 @@ func main() {
 		switch v := interfaceArr.(type) {
 		case []interface{}:
 			for x := 0; x < len(v); x++ {
-				topology = append(topology, v[x].(string))
+				neighbor := v[x].(string)
+				topology = append(topology, neighbor)
+				acked_map[neighbor] = -1
+				fmt.Fprintf(os.Stderr, "node: %s, neighbor: %s\n", n.ID(), neighbor)
+				c <- neighbor
 			}
 		default:
 			fmt.Printf("invalid type: %T\n", v)
@@ -102,16 +118,30 @@ func main() {
 		return n.Reply(msg, resp)
 	})
 
-	ticker := time.NewTicker(500 * time.Millisecond)
 	go func() {
-		for range ticker.C {
-			if len(queue) != 0 {
-				body := map[string]interface{}{"type": "broadcast_node", "messages": queue}
-				for _, neighbor := range topology {
-					n.Send(neighbor, body)
-				}
-				queue = make([]float64, 0)
+		for neighbor := range c {
+			go sleepChan(neighbor, c)
+			mu.Lock()
+			neighbor_ack := acked_map[neighbor]
+			if neighbor_ack == len(messages)-1 {
+				mu.Unlock()
+				continue
 			}
+			body := map[string]interface{}{"type": "broadcast_node", "messages": append([]float64{}, messages[neighbor_ack+1:]...)} // messages[neighbor_ack+1 : neighbor_ack+1+send_count]}
+			mu.Unlock()
+
+			neigh := neighbor
+			n.RPC(neigh, body, func(msg maelstrom.Message) error {
+				var body map[string]any
+				if err := json.Unmarshal(msg.Body, &body); err != nil {
+					return err
+				}
+				ack_msg := body["ack"].(float64)
+				mu.Lock()
+				acked_map[neigh] = max(message_map[ack_msg], acked_map[neigh])
+				mu.Unlock()
+				return nil
+			})
 		}
 	}()
 
