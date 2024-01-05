0f83f5c128a156d28cde26968026c826ca31ffc1
jbukl <148171160+jbukl@users.noreply.github.com>
2024-01-05 18:16:51 -0500

    challenge 4 almost fixed, but likely isn't done
    currently assumes the consistency guarantees imply that we're definitely going to see the answer by the end of the workload
    this doesn't seem to be the case, so it might make more sense to fetch local counters via RPCs
    we could have each node hold onto a map of the local counters, and have the nodes share them amongst each other, taking the largest of their copies
    will need locking

--- maelstrom-counter/main.go
+++ maelstrom-counter/main.go
@@ -3,16 +3,51 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+func initialize_list(ctx context.Context, kv *maelstrom.KV, n *maelstrom.Node) {
+	_ = kv.CompareAndSwap(ctx, "node_list", []string{}, []string{}, true)
+
+	for {
+		x, _ := kv.Read(ctx, "node_list")
+		interface_arr := x.([]interface{})
+
+		node_list := []string{}
+		for _, y := range interface_arr {
+			node_list = append(node_list, y.(string))
+		}
+		node_list = append(node_list, n.ID())
+
+		if err := kv.CompareAndSwap(ctx, "node_list", x, node_list, false); err == nil {
+			break
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func read_nodes(ctx context.Context, kv *maelstrom.KV) []string {
+	x, _ := kv.Read(ctx, "node_list")
+	interface_arr := x.([]interface{})
+
+	node_list := []string{}
+	for _, y := range interface_arr {
+		node_list = append(node_list, y.(string))
+	}
+	return node_list
+}
+
 func main() {
 	n := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(n)
 	ctx := context.Background()
+	node_counter := 0
+	list_initialized := false
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -21,14 +56,9 @@ func main() {
 		}
 
 		delta := int(body["delta"].(float64))
-		prevVal := 0
-		for {
-			err := kv.CompareAndSwap(ctx, "counter", prevVal, prevVal+delta, true)
-			if err == nil {
-				break
-			}
-			prevVal, _ = kv.ReadInt(ctx, "counter")
-			time.Sleep(10 * time.Millisecond)
+		node_counter += delta
+		if err := kv.Write(ctx, fmt.Sprintf("%s-counter", n.ID()), node_counter); err != nil {
+			return err
 		}
 
 		body["type"] = "add_ok"
@@ -43,11 +73,26 @@ func main() {
 			return err
 		}
 
-		_ = kv.CompareAndSwap(ctx, "counter", 0, 0, true)
-		val, _ := kv.ReadInt(ctx, "counter")
+		if !list_initialized {
+			initialize_list(ctx, kv, n)
+			list_initialized = true
+		}
 
+		node_list := read_nodes(ctx, kv)
+
+		read_total := 0
+		for _, neigh := range node_list {
+			if neigh == n.ID() {
+				read_total += node_counter
+				continue
+			}
+			val, err := kv.ReadInt(ctx, fmt.Sprintf("%s-counter", neigh))
+			if err == nil {
+				read_total += val
+			}
+		}
 		body["type"] = "read_ok"
-		body["value"] = val
+		body["value"] = read_total
 
 		return n.Reply(msg, body)
 	})
